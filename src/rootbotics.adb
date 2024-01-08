7b8dd3574c7a6ae25ecdfc247082d6da9837668d
Dylan E <76961140+KvanQuilst@users.noreply.github.com>
2024-01-08 12:16:03 -0800

    update docs

--- src/rootbotics.adb
+++ src/rootbotics.adb
@@ -38,7 +38,7 @@ with Root.Alliance;
 with Root.Lizards;
 
 procedure Rootbotics is
-   VERSION : constant String := "v0.1.2";
+   VERSION : constant String := "v0.2";
 
    -- In order of setup priority --
    type Faction is (Alliance, Lizards);
