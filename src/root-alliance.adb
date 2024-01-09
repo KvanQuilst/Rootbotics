55d9532550bb6dc3fb654aa91ea05100d7dffc18
KvanQuilst <eskew.dylan@outlook.com>
2024-01-08 18:37:49 -0800

    alliance: revolt

--- src/root-alliance.adb
+++ src/root-alliance.adb
@@ -25,6 +25,8 @@
 -------------------------------------------------------------------------------
 with Ada.Text_IO; use Ada.Text_IO;
 
+with Root.Maps; use Root.Maps;
+
 package body Root.Alliance is
 
    ---------------
@@ -106,7 +108,7 @@ package body Root.Alliance is
    ---------------
    -- Take Turn --
    ---------------
-   procedure Birdsong is null;
+   procedure Birdsong;
    procedure Daylight is null;
    procedure Evening  is null;
 
@@ -160,5 +162,68 @@ package body Root.Alliance is
       Continue;
 
    end Take_Turn;
+
+   procedure Revolt (Clears : Int_Arr; Time : Phase) is
+
+      function Count_Sym return Natural is
+         Count : Natural := 0;
+      begin
+         for C of Clears loop
+            if Map_Sympathy (C) then
+               Count := Count + 1;
+            end if;
+         end loop;
+         return Count;
+      end Count_Sym;
+
+      Num_Sym : constant Natural := Count_Sym;
+      Clear : Natural;
+      Opts  : String_Arr (1 .. Num_Sym + 1);
+      Idx   : Positive := 1;
+   begin
+      if Num_Sym = 0 then
+         return;
+      end if;
+
+      for C of Clears loop
+         if Map_Sympathy (C) then
+            Opts (Idx) := Unbounded (C'Image);
+            Idx := Idx + 1;
+         end if;
+      end loop;
+      Opts (Num_Sym + 1) := Unbounded ("None");
+
+      Prompt (Time);
+      Put_Line ("Which clearing has the most enemy pieces?");
+
+      Clear := Character'Pos (Get_Option (Opts)) - Character'Pos ('a') + 1;
+      if Clear <= Clears'Length then
+         Clear := Clears (Clear);
+         Prompt (Time);
+         Put_Line ("Remove all enemy pieces from clearing" & Clear'Image &
+                   ".");
+         Put_Line ("Place the" & Curr_Order'Image & " fort in clearing" &
+                   Clear'Image & ".");
+         Forts (Clear) := Forts (Clear) + 1;
+         Fort_Supply (Curr_Order) := Fort_Supply (Curr_Order) - 1;
+         Continue;
+      end if;
+   end Revolt;
+
+   procedure Birdsong is
+      Clears : constant Int_Arr := Filter_Clearings (Curr_Order);
+   begin
+      Prompt (Birdsong);
+      Put_Line ("Craft the order card for +1 points.");
+      Continue;
+
+      if Curr_Order /= Bird and then Fort_Supply (Curr_Order) > 0 then
+         Revolt (Clears, Birdsong);
+      else
+         --  TODO Public Pity
+         null;
+      end if;
+
+   end Birdsong;
 
 end Root.Alliance;
