ff119f007f03fdc517a658bbe7ed90bcf93c8bb6
KvanQuilst <eskew.dylan@outlook.com>
2024-01-15 12:30:58 -0800

    duchy: scaffolding

--- src/root-duchy.ads
+++ src/root-duchy.ads
@@ -37,14 +37,33 @@ package Root.Duchy is
 
 private
 
-   type Action is (Craft);
+   WARRIOR_MAX : constant Integer := 20;
+   CROWN_MAX   : constant Integer := 9;
+   TUNNEL_MAX  : constant Integer := 3;
+   BUILD_MAX   : constant Integer := 3;
+
+   type Action is (Craft, None);
+
+   procedure Put_Phase with Inline;
+   procedure Prompt    with Inline;
 
    function Unbounded (S : String) return Unbounded_String
       renames To_Unbounded_String;
 
+   Warrior_Sypply : Integer range 0 .. WARRIOR_MAX := WARRIOR_MAX;
+   Crown_Supply   : Integer range 0 .. CROWN_MAX   := CROWN_MAX;
+   Citadel_Supply : Integer range 0 .. BUILD_MAX   := BUILD_MAX;
+   Market_Supply  : Integer range 0 .. BUILD_MAX   := BUILD_MAX;
+   Tunnel_Supply  : Integer range 0 .. TUNNEL_MAX  := TUNNEL_MAX;
+
+   Map_Warriors   : Warrior_Arr   := (others => 0);
+   Map_Buildings  : Building_Arr  := (others => 0);
+   Map_Tunnels    : Token_Arr     := (others => False);
+   Rule           : Rule_Arr      := (others => False);
+
    Curr_Order  : Suit;
-   Curr_Phase  : Phase;
-   Curr_Action : Action;
+   Curr_Phase  : Phase  := None;
+   Curr_Action : Action := None;
 
    Logo_Width : constant := 24;
    Logo : Logo_Arr :=
